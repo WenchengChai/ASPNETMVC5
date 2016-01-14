ce91045707aa1f588652840af1cf359b71ef7003
Wencheng Chai <wencheng.chai@gmail.com>
2016-01-14 16:18:32 +1100

    Ch9-Final

--- AutomatedTellerMachine/Controllers/TransactionController.cs
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -86,5 +86,43 @@ namespace AutomatedTellerMachine.Controllers
             }
             return View();
         }
+
+        public ActionResult Transfer(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Transfer(TransferViewModel transfer)
+        {
+            // check for available funds
+            var sourceCheckingAccount = db.CheckingAccounts.Find(transfer.CheckingAccountId);
+            if (sourceCheckingAccount.Balance < transfer.Amount)
+            {
+                ModelState.AddModelError("Amount", "You have insufficient funds!");
+            }
+
+            // check for a valid destination account
+            var destinationCheckingAccount = db.CheckingAccounts.Where(c => c.AccountNumber == transfer.DestinationCheckingAccountNumber).FirstOrDefault();
+            if (destinationCheckingAccount == null)
+            {
+                ModelState.AddModelError("DestinationCheckingAccountNumber", "Invalid destination account number.");
+            }
+
+            // add debit/credit transactions and update account balances
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(new Transaction { CheckingAccountId = transfer.CheckingAccountId, Amount = -transfer.Amount });
+                db.Transactions.Add(new Transaction { CheckingAccountId = destinationCheckingAccount.Id, Amount = transfer.Amount });
+                db.SaveChanges();
+
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transfer.CheckingAccountId);
+                service.UpdateBalance(destinationCheckingAccount.Id);
+
+                return PartialView("_TransferSuccess", transfer);
+            }
+            return PartialView("_TransferForm");
+        }
     }
 }
