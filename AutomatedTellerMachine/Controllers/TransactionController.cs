302447d81626f9cde1915a372ff3db2d6d6ed3fc
Wencheng Chai <wencheng.chai@gmail.com>
2016-01-14 16:17:43 +1100

    Ch9-Start

--- AutomatedTellerMachine/Controllers/TransactionController.cs
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -42,5 +42,49 @@ namespace AutomatedTellerMachine.Controllers
             }
             return View();
         }
+
+        public ActionResult QuickCash(int checkingAccountId, decimal amount)
+        {
+            var sourceCheckingAccount = db.CheckingAccounts.Find(checkingAccountId);
+            var balance = sourceCheckingAccount.Balance;
+            if (balance < amount)
+            {
+                return View("QuickCashInsufficientFunds");
+            }
+            db.Transactions.Add(new Transaction { CheckingAccountId = checkingAccountId, Amount = -amount });
+            db.SaveChanges();
+
+            var service = new CheckingAccountService(db);
+            service.UpdateBalance(checkingAccountId);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Withdrawal(int checkingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Withdrawal(Transaction transaction)
+        {
+            var checkingAccount = db.CheckingAccounts.Find(transaction.CheckingAccountId);
+            if (checkingAccount.Balance < transaction.Amount)
+            {
+                ModelState.AddModelError("Amount", "You have insufficient funds!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                transaction.Amount = -transaction.Amount;
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
     }
 }
