f54584aff6dd2902a97996ced54a89ae8ec5793e
Wencheng Chai <wencheng.chai@gmail.com>
2016-01-14 16:03:25 +1100

    Ch5-Start

--- AutomatedTellerMachine/Models/Transaction.cs
+++ AutomatedTellerMachine/Models/Transaction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,7 +15,7 @@ namespace AutomatedTellerMachine.Models
         [DataType(DataType.Currency)]
         public decimal Amount { get; set; }
 
-        [Required]
+        [Required]               
         public int CheckingAccountId { get; set; }
         public virtual CheckingAccount CheckingAccount { get; set; }
     }
