e7f002da40de93d62f577a7e5d63e205e7260399
Wencheng Chai <wencheng.chai@gmail.com>
2016-01-14 16:02:38 +1100

    Ch4-Final

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -35,5 +35,7 @@ namespace AutomatedTellerMachine.Models
 
         [Required]
         public string ApplicationUserId { get; set; }
+
+        public virtual ICollection<Transaction> Transactions { get; set; } 
     }
 }
